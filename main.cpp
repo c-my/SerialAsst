fc79c2f3271619db623c8707ed9790ce36da01c3
才明洋 <cmy1113@yeah.net>
2018-02-10 20:04:01 +0800

    取消图标

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setAttribute(Qt::AA_UseHighDpiPixmaps);
     a.setWindowIcon(QIcon(QString(":/pic/icon/icon.png")));
     MainWindow w;
     w.show();
